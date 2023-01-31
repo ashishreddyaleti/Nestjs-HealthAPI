e8d76bb3575cb89d4491e9ed08f9097c53eeef60
ashishreddyaleti <35349499+ashishreddyaleti@users.noreply.github.com>
2023-01-31 12:36:32 -0800

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('devops');
   await app.listen(80);
 }
 bootstrap();
